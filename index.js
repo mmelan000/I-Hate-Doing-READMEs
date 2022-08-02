b2d7f52fdd8bcb7e615a4d4b5796639aea71308b
Cloud <m.melanson000@gmail.com>
2022-08-02 16:48:04 -0400

    finished questions

--- index.js
+++ index.js
@@ -3,7 +3,105 @@ const fs = require('fs/promises');
 const inquirer = require('inquirer');
 const generateMD = require('./utils/generateMarkdown.js');
 // TODO: Create an array of questions for user input
-const questions = ['Please enter a project title:', ''];
+const questions = [{
+    type: 'input',
+    name: 'title',
+    message: "Please enter your project's title:",
+    validate(value) {
+        if (value) {
+            return true;
+        }
+        return 'Please enter a title.'
+    }
+},
+{
+    type: 'input',
+    name: 'description',
+    message: "Please enter your project's description:",
+    validate(value) {
+        if (value) {
+            return true;
+        }
+        return 'Please enter a description.'
+    }
+},
+{
+    type: 'input',
+    name: 'installation',
+    message: "Please enter instructions for installation:",
+    validate(value) {
+        if (value) {
+            return true;
+        }
+        return 'Please enter instructions for installation.'
+    }
+},
+{
+    type: 'input',
+    name: 'usage',
+    message: "Please provide an example of use:",
+    validate(value) {
+        if (value) {
+            return true;
+        }
+        return 'Please enter an example.'
+    }
+},
+{
+    type: 'list',
+    name: 'license',
+    message: 'What license will you be using?',
+    choices: [
+        'BSD-2',
+        'BSD-3',
+        'GNU',
+        'MIT'
+    ],
+},
+{
+    type: 'input',
+    name: 'contribution',
+    message: "Please describe standards for contribution:",
+    validate(value) {
+        if (value) {
+            return true;
+        }
+        return 'Please enter a description of the standards for contribution.'
+    }
+},
+{
+    type: 'input',
+    name: 'testing',
+    message: "How should users test the program?",
+    validate(value) {
+        if (value) {
+            return true;
+        }
+        return 'Please enter an example of how to test the program.'
+    }
+},
+{
+    type: 'input',
+    name: 'github',
+    message: "Enter Github U/N:",
+    validate(value) {
+        if (value) {
+            return true;
+        }
+        return 'Please enter your Github Username.'
+    }
+},
+{
+    type: 'input',
+    name: 'email',
+    message: "Enter email address:",
+    validate(value) {
+        if (value) {
+            return true;
+        }
+        return 'Please enter an email address.'
+    }
+},];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
@@ -12,54 +110,11 @@ function writeToFile(fileName, data) {
 
 // TODO: Create a function to initialize app
 function init() {
-    // Title 
-        // Question
-        // Apply
-
-    // Description
-        // Question
-        // Apply
-    
-    // Table of Contents
-        // Apply
-    
-    // Installation
-        // Question
-        // Apply
-
-    // Usage
-    
-    // License
-        // List
-        inquirer
-        .prompt([
-          {
-            type: 'list',
-            name: 'license',
-            message: 'What license will you be using?',
-            choices: [
-              'BSD-2',
-              'BSD-3',
-              'GNU',
-              'MIT'
-            ],
-          },
-        ])
+    inquirer
+        .prompt(questions)
         .then((answers) => {
-            console.log(answers.license);
+            console.log(answers);
         });
-        // Add Badge
-        // Apply - a notice is added to the section of the README entitled License that explains which license the application is covered under
-    
-    // Contributing
-    
-    // Tests
-    
-    // Questions
-        // query Github U/N
-            // apply
-        // query email
-            //apply
 }
 
 
