ab47e89dc321dac161455d6f0960d31f824d7847
Cloud <m.melanson000@gmail.com>
2022-08-02 10:51:53 -0400

    commented out todos into smaller pieces

--- index.js
+++ index.js
@@ -1,13 +1,50 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs/promises');
+const inquirer = require('inquirer');
+const generateMD = require('./utils/generateMarkdown.js');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ['titleQ', ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    // Title 
+        // Question
+        // Apply
+
+    // Description
+        // Question
+        // Apply
+    
+    // Table of Contents
+        // Apply
+    
+    // Installation
+        // Question
+        // Apply
+
+    // Usage
+    
+    // License
+        // List
+        // Add Badge
+        // Apply - a notice is added to the section of the README entitled License that explains which license the application is covered under
+    
+    // Contributing
+    
+    // Tests
+    
+    // Questions
+        // query Github U/N
+            // apply
+        // query email
+            //apply
+}
+
 
 // Function call to initialize app
 init();
