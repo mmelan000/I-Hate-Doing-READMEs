af7477daa2089698b13ff98c30cf7d4b37119635
Cloud <m.melanson000@gmail.com>
2022-08-03 09:46:47 -0400

    completed init function and writeToFile Function

--- index.js
+++ index.js
@@ -105,16 +105,22 @@ const questions = [{
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-
-}
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+            console.log(err)
+        }
+        else {
+            console.log(`${fileName} has successflly been created.`)
+        }
+    })
+};
 
 // TODO: Create a function to initialize app
 function init() {
     inquirer
         .prompt(questions)
-        .then((answers) => {
-            console.log(answers);
-        });
+        .then((answers) => {generateMD(answers)})
+        .then((data) =>{writeToFile('README.md', data)});
 }
 
 
